0ed895b015ec397937bde0e20991dc81a9ea0772
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2022-12-19 11:48:20 +0530

    Added file dbms showui

--- Student_Management_Console/src/Student.java
+++ Student_Management_Console/src/Student.java
@@ -1,4 +1,68 @@
 public class Student {
-    String Rollno;
+    String RollNo;
     String Name;
+    String Address;
+    String Standard;
+    String DOB;
+    String ContactNo;
+
+    public String getRollNo() {
+        return RollNo;
+    }
+
+    public void setRollNo(String rollNo) {
+        RollNo = rollNo;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public String getStandard() {
+        return Standard;
+    }
+
+    public void setStandard(String standard) {
+        Standard = standard;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getContactNo() {
+        return ContactNo;
+    }
+
+    public void setContactNo(String contactNo) {
+        ContactNo = contactNo;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "RollNo='" + RollNo + '\'' +
+                ", Name='" + Name + '\'' +
+                ", Address='" + Address + '\'' +
+                ", Standard='" + Standard + '\'' +
+                ", DOB='" + DOB + '\'' +
+                ", ContactNo='" + ContactNo + '\'' +
+                '}';
+    }
 }
---
+++ Student_Management_Console/src/UserInterface.java
@@ -0,0 +1,5 @@
+public class UserInterface {
+    public static void ShowMain(){
+        System.out.println(("What do you want to do?\n 1. View all the Students\n 2. Register a Student \n 3. Update a Student \n 3. Delete a Student"));
+    }
+}
